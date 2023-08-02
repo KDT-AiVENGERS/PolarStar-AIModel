fa4a57831c0633354c9baa2dfc49a812b1cd0142
kwakchanhyuk <kwakchanhyuk@naver.com>
2023-08-02 20:15:20 +0900

    [#8]FEAT: utils/handing_request.py의 find_matched_jds함수 작성

--- utils/handling_requests.py
+++ utils/handling_requests.py
@@ -1,13 +1,15 @@
 import torch
 
-def find_matched_jds(v_jd, jds, k = 20, target_columns = ['자격요건', '우대조건', '복지', '회사소개', '주요업무']) -> list:
-    '''
-    inputs
-      v_jd: 가상 jd 의 torch 임베딩 벡터 (1 x 3840)
-      jds: 전체 jd 의 torch 임베딩 벡터 (n x 3840)
-      target_columns: 비교 대상 항목
-    output
-      가상 jd 와 연관성이 높은 순서대로 k 개의 index(jds 의 index) 의 리스트
-    '''
+def find_matched_jds(vec_mock, vec_origin, start, end, target_columns = ['자격요건', '우대조건', '복지', '회사소개', '주요업무']) -> list:
+    
+    result = []
+    col_dic = {'자격요건':0,'우대조건':1,'복지':2,'회사소개':3,'주요업무':4}
+    col_list = [col_dic.get(col) for col in target_columns]
 
-    pass
+    mocks = torch.chunk(vec_mock, chunks=5, dim=0)
+    origins = torch.chunk(vec_origin, chunks=5, dim=1)
+    
+    for col_idx in col_list:
+        result.append(torch.nn.functional.cosine_similarity(mocks[col_idx].unsqueeze(0),origins[col_idx], dim=1))
+
+    return (sum(result)/3).argsort()[start:end]
