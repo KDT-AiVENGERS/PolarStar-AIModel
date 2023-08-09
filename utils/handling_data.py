e3a0c8799b1e14432f4bccde4423288aad0b70b7
dkqp <ltg0513@gmail.com>
2023-08-09 14:10:48 +0900

    [#29]FIX: generage_v_jd function fix

--- utils/handling_data.py
+++ utils/handling_data.py
@@ -110,101 +110,38 @@ def embedding_v_jds(v_jd_dict, target_columns = ['자격요건', '우대조건',
   return output_pooler.view((1, -1))
 
 def generate_v_jd(QnA_answer: dict):
-  jd_dic = {'자격요건':'','우대조건':'','복지':'수습기간 급여지급\n수습기간 적용\n','회사소개':'','주요업무':''}
+  jd_dic = {'자격요건':'','우대조건':'','복지':'수습','회사소개':'','주요업무':''}
 
-  for question, answer in QnA_answer.items():
+  for key, value in QnA_answer.items():
+    question = key
+    answer = ' '.join(value)
 
-        match question:
-            case 'personality':
-                for i in answer:
-                        jd_dic['자격요건'] += (i + '능력을 가지고 계신분\n'
-                                            )
+    match question:
+      case 'personality':
+        jd_dic['자격요건'] = jd_dic['자격요건'] + answer +' '
+        jd_dic['회사소개'] = jd_dic['회사소개'] + answer +' '
 
-                        jd_dic['우대조건'] += (i + '능력을 가지고 계신분\n'
-                                            )
+      case 'stack':
+        if len(answer) == 0:
+          pass
 
-                        jd_dic['회사소개'] += (i + '문화를 가지고 있는 회사 입니다.\n'
-                                           )
+        else:
+          jd_dic['자격요건'] = jd_dic['자격요건'] + answer +' '
+          jd_dic['우대조건'] = jd_dic['우대조건'] + answer +' '
 
-            case 'stack':
-                if len(answer) == 0:
-                    pass
+      #default '수습' in welfare
+      case 'welfare':
+        if '수습' not in answer:
+          jd_dic['복지'] = answer
+        else:
+          jd_dic['복지'] = jd_dic['복지'] + answer +' '
 
-                else:
-                    for i in answer:
-                        jd_dic['자격요건'] += (i + '개발 경험이 있으신 분\n'
-                                            + i + '개발 경험\n'
-                                            + i + '사용이 가능하신 분\n'
-                                            + i + '에대한 이해\n'
-                                            + i + '에 관심이 있으신 분'
-                                            + '기술스택: ' + ','.join(i)
-                                            )
-
-                        jd_dic['우대조건'] += (i + '사용에 능숙하신 분\n'
-                                           + i + '개발 경험이 있으신 분\n'
-                                           + i + '개발 경험'
-                                           )
-
-            #default '수습' in welfare
-            case 'welfare':
-                if '수습' not in answer:
-                    jd_dic['복지'] = ''
-
-                if '장비' in answer:
-                    jd_dic['복지'] += '필요한 장비지원\n장비 구매 지원\n'
-
-                if '휴가' in answer:
-                     jd_dic['복지'] += '휴가 지원\n연차\n'
-
-                if '출퇴근' in answer:
-                     jd_dic['복지'] += '유연근무제를 시행중입니다.\n재택근무를 시행중\n'
-
-                if '식사' in answer:
-                     jd_dic['복지'] += '구내식당, 사내식당\n중/석식비 제공, 식사비 제공\n'
-
-                if '건강검진' in answer:
-                     jd_dic['복지'] += '건강검진 지원\n '
-
-                if '인센티브' in answer:
-                     jd_dic['복지'] += '인센티브 제공\n스톡옵션 제공\n성과에 따른 보상 제공\n'
-
-                if '계발' in answer:
-                     jd_dic['복지'] += '온라인강의 제공\n 도서, 시험지 제공\n자기계발비 지원'
+      case 'job':
+        jd_dic['주요업무'] = jd_dic['주요업무'] + answer +' '
 
 
-            case 'job':
-                if len(answer) == 0:
-                    pass
-
-                for i in answer:
-                        jd_dic['자격요건'] += (i + '관련 프로젝트 경험이 있는분\n'
-                                            + i + '에 대한 지식을 보유하고 있는 분\n'
-                                            + i + '관련 지식 보유\n'
-                                            )
-
-                        jd_dic['주요업무'] += (i + '관련 연구 및 개발\n'
-                                           )
-
-                        jd_dic['우대조건'] += (i + '관련 프로젝트 경험이 있는분\n'
-                                            + i + '에 대한 지식을 보유하고 있는 분\n'
-                                            + i + '관련 지식 보유\n'
-                                           )
-
-
-            case 'domain':
-
-                for i in answer:
-                    jd_dic['회사소개'] += (i + '관련 프로젝트 경험이 있는분\n'
-                                        + i + '에 대한 지식을 보유하고 있는 분\n'
-                                        + i + '관련 지식 보유\n'
-                                        )
-
-                    jd_dic['우대조건'] += (i + '에 대한 지식을 보유하고 있는 분\n'
-                                        + i + '관련 지식 보유\n'
-                                        )
-
-                    jd_dic['주요업무'] += (i + '관련 연구 및 개발\n'
-                                           )
+      case 'domain':
+        jd_dic['회사소개'] = jd_dic['회사소개'] + answer +' '
 
   return jd_dic
 
